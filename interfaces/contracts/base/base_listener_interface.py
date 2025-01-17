d961aae8ab080b923458352db7c60d2b4181e722
j1m5s3 <james.lynch.uml@gmail.com>
2025-01-16 22:08:58 -0600

    added asyncio run

--- interfaces/contracts/base/base_listener_interface.py
+++ interfaces/contracts/base/base_listener_interface.py
@@ -12,18 +12,20 @@ class BaseListenerInterface(WebSocketProvider):
     """
 
     event: bytes = None
+    event_name: str = None
     contract_address: str = None
     filter_params: dict = {
         "address": None,
         "topics": None
     }
 
-    logger: Logger = Logger(section_name=f"{__name__}")
+    logger: Logger = None
 
     def __init__(
             self,
             ws_uri: str,
             event: str,
+            event_name: str,
             contract_address: str
     ):
         """
@@ -31,16 +33,18 @@ class BaseListenerInterface(WebSocketProvider):
 
         :param ws_uri: String URI for websocket
         :param event: String event name with param types Ex. "EventName(address, uint256)"
+        :param event_name: String event name
         :param contract_address:
         """
         self.event = Web3.keccak(text=event)
         self.contract_address = contract_address
         self.filter_params["address"] = contract_address
         self.filter_params["topics"] = [event]
+        self.logger = Logger(section_name=f"{event_name}_listener")
 
         super().__init__(ws_uri)
 
-    async def subscribe_to_event(self) -> None:
+    async def _subscribe_to_event(self) -> None:
         """
         Subscribe to event
 
@@ -58,3 +62,14 @@ class BaseListenerInterface(WebSocketProvider):
             async for event in w3.socket.process_subscriptions():
                 decoded_event = decode(event["data"], [self.event])
                 self.logger.info(f"Event: {decoded_event}")
+        return
+
+    def run(self) -> None:
+        """
+        Run listener
+
+        :return:
+        """
+        self.logger.info("Running listener")
+        asyncio.run(self._subscribe_to_event())
+        return
